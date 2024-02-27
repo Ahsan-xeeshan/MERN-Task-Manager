c834f9dea56325d12fd3d62ae5871c527cae71e1
Nazmul Ahsan <xeeshann.ahsan@gmail.com>
2024-02-28 01:42:22 +0600

    render server

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://mern-task-manager-ihqc.onrender.com",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
